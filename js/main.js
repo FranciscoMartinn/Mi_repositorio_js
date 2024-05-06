85c10f869cc039b24293fe933c4d1bf22a8df37a
FranciscoMartinn <franmartiin09@gmail.com>
2024-05-06 14:17:16 -0300

    primer repositorio

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let edad = prompt("Ingresa tu edad")
+
+if (edad <= 10) {
+    console.log("Sos un nene, no podes ingresar, vete a jugar!");
+} else if (edad <= 18) {
+    console.log("Bienvenido jovencito, espero disfrute de PIZZA HOUSE!");
+} else if (edad <= 29) {
+    console.log("Bienvenido joven, espero disfrute de PIZZA HOUSE!");
+} else if (edad >= 30) {
+    console.log("Bienvenido señor, espero disfrute de PIZZA HOUSE!");
+}
+
+
+let cantidadPizza = parseInt(prompt("Ingrese la cantidad de pizzas que desee"));
+let x = 1;
+while (x <= cantidadPizza) {
+    let pizza = prompt("Ingrese el nombre de la pizza que desee");
+    console.log(x + ": " + pizza);
+    x = x + 1;
+}
