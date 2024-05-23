d4a1dd3c2abbae8934305f0bf050a37446e4b4e7
FranciscoMartinn <franmartiin09@gmail.com>
2024-05-23 11:26:51 -0300

    act

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+/*--FUNCION--*/
+
 // function saludar(nombre) {
 //     console.log("Hola, " + nombre + "!");
 // }
 const saludar = (nombre) => {
     console.log ("Hola, " + nombre + "!")
-}
+};
 
 let nombreIngresado = prompt ("Ingresa tu nombre");
 saludar(nombreIngresado);
@@ -19,7 +21,7 @@ if (edad <= 10) {
     console.log("Bienvenido joven, espero disfrute de PIZZA HOUSE!");
 } else if (edad >= 30) {
     console.log("Bienvenido señor, espero disfrute de PIZZA HOUSE!");
-}
+};
 
 
 let cantidadPizza = parseInt(prompt("Ingrese la cantidad de pizzas que desee"));
@@ -28,7 +30,9 @@ while (x <= cantidadPizza) {
     let pizza = prompt("Ingrese el nombre de la pizza que desee");
     console.log(x + ": " + pizza);
     x = x + 1;
-}
+};
+
+/*--OBJETO--*/
 
 const usuario1 = {
     nombre: "Francisco",
@@ -42,10 +46,19 @@ const usuario1 = {
     }
 };
 
-//console.log(usuario1.edad);
+// console.log(usuario1.edad);
 
 usuario1["edad"],
 console.log(usuario1.edad);
 
 usuario1.localidad = "General Alvear";
 console.log(usuario1);
+
+/*--ARRAY--*/
+
+let productos = ["Muzzarella", "Especial", "Calabresa", "Rucula"];
+
+productos.push("Cebolla Caramelizada");
+console.log(productos);
+
+console.log(productos.length);
